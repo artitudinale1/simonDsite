ae7c04e1fe63d0981bf61461d2600353f527befc
Alex <user@users-imac.home>
2014-10-07 10:36:31 +0100

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,51 @@
+ $( document ).ready(function() {
+ 	 var width = $(window).width();
+ 	 console.log(width)
+ 	 	if (width< 767){
+	    console.log('gg')
+	    $( "#nav" ).hide();
+	    	$('#mobile_menu').click(function() {
+	    		$( "#nav" ).slideToggle( "slow" );
+
+	    		console.log('gvvg')
+	    	
+	    	});
+	    }else{
+	    	 $( "#nav" ).show();
+	    }
+
+
+$( "#submit" ).click(function()  {
+	if ( $( "input:first" ).val() === "correct" ) {
+	$('#logos').css('display','none');
+	$('#isotope').css('display','block');
+	$('#pass').fadeOut('slow');
+	$('.item').css('height' ,'498px;')
+}
+else{
+	alert("password not valid")
+}
+});
+
+
+	    });
+
+
+	    $( window ).resize(function() {
+	    		 var width = $(window).width();
+ 				 console.log(width)
+ 	 			if (width< 767){
+	   				 console.log('gg')
+	   			 $( "#nav" ).hide();
+	    			$('#mobile_menu').unbind('click').click(function(){
+	    				$( "#nav" ).slideToggle( "slow" );
+	    				console.log('gg')
+	    	
+	    			})
+	    		}else{
+	    	 $( "#nav" ).show();
+	    }
+
+
+	    });
+
