59037196bb7bb4bd2ad49d23966ddc7b6e1a7def
Alex <alex.garulli@gmail.com>
2014-11-24 03:50:13 +0000

    tidy mani.js and CSS fix for tablet and  mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,169 +1,107 @@
  $( document ).ready(function() {
 
+//GUIDELINE TOOGLE
+      $( "#guidelines" ).click(function() {
+           $("*").toggleClass( "outline" );
+      });
 
-
-
-
-
-
-
-
-
-
-
-$( "#guidelines" ).click(function()  {
-	
- 	$("*").toggleClass( "outline" );
-  
-});
-
-
-
- 	 var width = $(window).width();
- 	 console.log(width)
- 	 	if (width< 767){
-	    console.log('gg')
+//CALCULATE WITH TO DISPLAY MOBILE NAV INSTEAD OD DT ONE
+  var width = $(window).width();
+ 	console.log(width) //NOT TOO SURE JQUERY GOT THIS RIGHT!!!!!!!
+ 	if (width< 758){
 	    $( "#nav" ).hide();
-	    	$('#mobile_menu').click(function() {
-	    		$( "#nav" ).slideToggle( "slow" );
-
-	    		console.log('gvvg')
-	    	
+	    $('#mobile_menu').click(function() {
+	    		$( "#nav" ).slideToggle( "slow" ); 	
 	    	});
 	    }else{
 	    	 $( "#nav" ).show();
 	    }
 
+
+//JS FOR WORK PAGE: ISOTOPE + PASSWORD VERIFICATION
 var $container = $('#container');
-
-
-
 $( "#submit" ).click(function()  {
-	if ( $( "input:first" ).val() === "correct" ) {
-		//console.log('dui')
-	$('#logos').css('display','none');
-	$('#isotope').css('display','block');
-	$('#pass').fadeOut('slow');
-	//$('.item').css('height' ,'498px;')
+    	if ( $( "input:first" ).val() === "correct" ) {
+    		      	$('#logos').css('display','none');
+                $('#isotope').css('display','block');
+                $('#pass').fadeOut('slow');
+                // init
+                $container.css('top', '100px');
+                $container.isotope({
+                    // options
+                    itemSelector: '.item',
+                    masonry: {
+                        columnWidth: 5
+                    }
+                });
 
+                $('.item').on('click', '.close', function() {
+                    $('.item').removeClass('large');
+                    $('.my-slidershow').css('display', 'none');
+                    $('.item').find('.more-detail').css('display', 'none');
+                    $('.item').find('.thumbs').css('display', 'block');
+                    $container.isotope('layout');
+                    return false
+                });
 
+                $container.on('click', '.item', function(){
+                  var thisEle = $(this);
+                  $('.item').not(this).find('.more-detail').css('display', 'none');
+                  $('.item').removeClass('large');
+                  $('.item').removeClass('large');
+                  $('.my-slidershow').css('display', 'none');
+                  $('.item').find('.more-detail').css('display', 'none');
+                  $('.item').find('.thumbs').css('display', 'block');
+                  $container.isotope('layout');
+                  $('.item').not(this).find('.thumbs').css('display', 'block');
+                  $('.item').not(this).removeClass('large');
+                  $(this).addClass('large');
+              	  $container.isotope('layout');
 
-// init
-$container.css('top', '100px');
-$container.isotope({
-  // options
-  itemSelector: '.item',
-    masonry: {
-    columnWidth: 5
-  }
+               	  if ( $(this).hasClass('large')) {
+                        $(this).find('.more-detail').css('display', 'block');
+                        $(this).find('.more-detail').show();
+           	  	        $(this).find('.thumbs').css('display', 'none');
+           	  	        $(this).find('.my-slidershow').css('visibility', 'visible');
+                        $(this).find('.my-slidershow').css('display', 'block');
+           	  	        $container.isotope('layout');
+           	      } else {
+           	  	        $(this).find('.more-detail').css('display', 'none');
+           	  	        $(this).find('.thumbs').css('display', 'block');
+                      }
+           	      $container.isotope('layout');
+                });
+            }else{
+          	alert("password not valid")
+            }
+      });//END SUBMIT FUNCTION
 
-});
-
-
-
- $('.item').on('click', '.close', function() {
-    //  alert('dui')
-       $('.item').removeClass('large');
- $('.my-slidershow').css('display', 'none');
- $('.item').find('.more-detail').css('display', 'none');
-            $('.item').find('.thumbs').css('display', 'block');
-          $container.isotope('layout');
-          return false
-       //  $('.item').addClass('item');
+       //GALLERIES ON EACH ISOTOPE ELEMENTS
+      $('.item').find('.my-slidershow').each(function (i) {
+          $(this).bjqs({       
+              'width' : 600,
+              'height' : 600,
+              'responsive' : true,
+              'usecaptions' : true, // show captions for images using the image title tag
+              'nexttext' : '<img class="gallery-controller" src="../images/next.png"/>', // Text for 'next' button (can use HTML)
+              'prevtext' : '<img class="gallery-controller" src="../images/prev.png"/>', // Text for 'previous' button (can use HTML)
+              'automatic' : true, // automatic
+              'animspeed' : 2000,
             });
+         });
+}); // END DOC READY FUNCTION
 
-
-
-
-   $container.on('click', '.item', function(){
-
-
-
-
-
-
-        var thisEle = $(this);
-          $('.item').not(this).find('.more-detail').css('display', 'none');
-          $('.item').removeClass('large');
-            $('.item').removeClass('large');
- $('.my-slidershow').css('display', 'none');
- $('.item').find('.more-detail').css('display', 'none');
-            $('.item').find('.thumbs').css('display', 'block');
-          $container.isotope('layout');
-         
-          $('.item').not(this).find('.thumbs').css('display', 'block');
-       	  //$(this).css('width', '600px')
-       	//   $('.item').not(this).find('.my-slidershow').css('display', 'none');
-       	  $('.item').not(this).removeClass('large');
-          console.log('reset')
-           $(this).addClass('large');
-       	  $container.isotope('layout');
-
-       	  if ( $(this).hasClass('large')) {
-
-         
-
-
-$(this).find('.more-detail').css('display', 'block');
-
-       	  	$(this).find('.more-detail').show();
-       	  	 $(this).find('.thumbs').css('display', 'none');
-       	  	  $(this).find('.my-slidershow').css('visibility', 'visible');
-               $(this).find('.my-slidershow').css('display', 'block');
-       	  	$container.isotope('layout');
-             console.log('open')
-       	  } else {
-       	  	$(this).find('.more-detail').css('display', 'none');
-       	  	$(this).find('.thumbs').css('display', 'block');
-             console.log('all close')
-       	  }
-       	 $container.isotope('layout');
-       	});
-
-
-  
-   
-}
-else{
-	alert("password not valid")
-}
-});
-
-
-   $('.item').find('.my-slidershow').each(function (i) {
-    $(this).bjqs({
-       
-        'width' : 600,
-         'height' : 600,
-        'responsive' : true,
-        'usecaptions' : true, // show captions for images using the image title tag
-        'nexttext' : '<img class="gallery-controller" src="../images/next.png"/>', // Text for 'next' button (can use HTML)
-        'prevtext' : '<img class="gallery-controller" src="../images/prev.png"/>', // Text for 'previous' button (can use HTML)
-        'automatic' : true, // automatic
-        'animspeed' : 2000,
-    });
-   });
-
-   
-
-	    });
-
-
-	    $( window ).resize(function() {
-	    		 var width = $(window).width();
- 				 console.log(width)
- 	 			if (width< 767){
-	   				 console.log('gg')
-	   			 $( "#nav" ).hide();
-	    			$('#mobile_menu').unbind('click').click(function(){
-	    				$( "#nav" ).slideToggle( "slow" );
-	    				console.log('gg')
-	    	
-	    			})
-	    		}else{
+//WATCH RESIZE WINDOW FUNCTION
+$( window ).resize(function() {
+	 var width = $(window).width();
+ 	 console.log(width)
+ 	 if (width< 758){
+	   		$( "#nav" ).hide();
+	    	$('#mobile_menu').unbind('click').click(function(){
+	    	     $( "#nav" ).slideToggle( "slow" );
+	        })
+	 }else{
 	    	 $( "#nav" ).show();
 	    }
-
-
-	    });
+});
 
