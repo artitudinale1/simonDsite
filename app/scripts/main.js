edb318eeb1f6e0d21b62d36eb9da49a3b3f2c439
Alex <alex.garulli@gmail.com>
2014-11-30 20:34:52 +0000

    CSS tweaks: footer and about page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,23 +7,23 @@
 
 //CALCULATE WITH TO DISPLAY MOBILE NAV INSTEAD OD DT ONE
   var width = $(window).width();
- 	console.log(width) //NOT TOO SURE JQUERY GOT THIS RIGHT!!!!!!!
- 	if (width< 758){
-	    $( "#nav" ).hide();
-	    $('#mobile_menu').click(function() {
-	    		$( "#nav" ).slideToggle( "slow" ); 	
-	    	});
-	    }else{
-	    	 $( "#nav" ).show();
-	    }
+  console.log(width) //NOT TOO SURE JQUERY GOT THIS RIGHT!!!!!!!
+  if (width< 758){
+      $( "#nav" ).hide();
+      $('#mobile_menu').click(function() {
+          $( "#nav" ).slideToggle( "slow" );  
+        });
+      }else{
+         $( "#nav" ).show();
+      }
 
 
 //JS FOR WORK PAGE: ISOTOPE + PASSWORD VERIFICATION
 var $container = $('#container');
 $( "#submit" ).click(function()  {
-    	if ( $( "input:first" ).val() === "correct" ) {
-    		      	$('#logos').css('display','none');
-                $('#isotope').css('display','block');
+      if ( $( "input:first" ).val() === "correct" ) {
+                $('.fourth').css('display','none');
+                $('.item').css('display','inline-block');
                 $('#pass').fadeOut('slow');
                 // init
                 $container.css('top', '100px');
@@ -56,24 +56,24 @@ $( "#submit" ).click(function()  {
                   $('.item').not(this).find('.thumbs').css('display', 'block');
                   $('.item').not(this).removeClass('large');
                   $(this).addClass('large');
-              	  $container.isotope('layout');
+                  $container.isotope('layout');
 
-               	  if ( $(this).hasClass('large')) {
+                  if ( $(this).hasClass('large')) {
                         $(this).find('.more-detail').css('display', 'block');
                         $(this).find('.more-detail').show();
-           	  	        $(this).find('.thumbs').css('display', 'none');
+                        $(this).find('.thumbs').css('display', 'none');
                         $(this).find('.close').css('display', 'block');
-           	  	        $(this).find('.my-slidershow').css('visibility', 'visible');
+                        $(this).find('.my-slidershow').css('visibility', 'visible');
                         $(this).find('.my-slidershow').css('display', 'block');
-           	  	        $container.isotope('layout');
-           	      } else {
-           	  	        $(this).find('.more-detail').css('display', 'none');
-           	  	        $(this).find('.thumbs').css('display', 'block');
+                        $container.isotope('layout');
+                  } else {
+                        $(this).find('.more-detail').css('display', 'none');
+                        $(this).find('.thumbs').css('display', 'block');
                       }
-           	      $container.isotope('layout');
+                  $container.isotope('layout');
                 });
             }else{
-          	alert("password not valid")
+            alert("password not valid")
             }
       });//END SUBMIT FUNCTION
 
@@ -84,8 +84,8 @@ $( "#submit" ).click(function()  {
 //              'height' : 381,
              //'responsive' : true,
               'usecaptions' : true, // show captions for images using the image title tag
-              'nexttext' : '<img class="gallery-controller" src="../images/next.png"/>', // Text for 'next' button (can use HTML)
-              'prevtext' : '<img class="gallery-controller" src="../images/prev.png"/>', // Text for 'previous' button (can use HTML)
+              'nexttext' : '<img class="gallery-controller" src="/static/images/next.png"  />', // Text for 'next' button (can use HTML)
+              'prevtext' : '<img class="gallery-controller" src="/static/images/prev.png"  />', // Text for 'previous' button (can use HTML)
               'automatic' : true, // automatic
               'animspeed' : 2000,
             });
@@ -94,15 +94,14 @@ $( "#submit" ).click(function()  {
 
 //WATCH RESIZE WINDOW FUNCTION
 $( window ).resize(function() {
-	 var width = $(window).width();
- 	 console.log(width)
- 	 if (width< 758){
-	   		$( "#nav" ).hide();
-	    	$('#mobile_menu').unbind('click').click(function(){
-	    	     $( "#nav" ).slideToggle( "slow" );
-	        })
-	 }else{
-	    	 $( "#nav" ).show();
-	    }
+   var width = $(window).width();
+   console.log(width)
+   if (width< 758){
+        $( "#nav" ).hide();
+        $('#mobile_menu').unbind('click').click(function(){
+             $( "#nav" ).slideToggle( "slow" );
+          })
+   }else{
+         $( "#nav" ).show();
+      }
 });
-
